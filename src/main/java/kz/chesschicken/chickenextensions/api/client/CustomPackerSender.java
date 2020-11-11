329481cde333f6c7805ece519449374221bcdc00
ChessChicken-Ru <53552480+ChessChicken-Ru@users.noreply.github.com>
2020-11-11 22:47:20 +0600

    Updating to 0.0.3. 10th part. Final!

--- src/main/java/kz/chesschicken/chickenextensions/api/client/CustomPackerSender.java
+++ src/main/java/kz/chesschicken/chickenextensions/api/client/CustomPackerSender.java
@@ -3,6 +3,8 @@ package kz.chesschicken.chickenextensions.api.client;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.launch.FabricServerTweaker;
+import net.fabricmc.loader.launch.server.FabricServerLauncher;
 import net.minecraft.entity.player.PlayerBase;
 import net.minecraft.packet.AbstractPacket;
 import net.minecraft.server.MinecraftServer;
@@ -10,6 +12,7 @@ import net.modificationstation.stationloader.api.common.event.packet.PacketRegis
 import net.modificationstation.stationloader.api.common.factory.GeneralFactory;
 import net.modificationstation.stationloader.api.common.packet.CustomData;
 import net.modificationstation.stationloader.api.common.packet.PacketHelper;
+import net.modificationstation.stationloader.api.common.registry.ModIDRegistry;
 import uk.co.benjiweber.expressions.functions.QuadConsumer;
 
 import java.util.List;
@@ -18,7 +21,6 @@ import java.util.function.BiConsumer;
 
 public class CustomPackerSender implements PacketRegister {
     public static String[] staticPlayerList;
-    private final MinecraftServer minecraftServer = (MinecraftServer) FabricLoader.getInstance().getGameInstance();
 
 
 
@@ -27,7 +29,6 @@ public class CustomPackerSender implements PacketRegister {
     public static void queue_PacketGetList(String s)
     {
         CustomData packet = GeneralFactory.INSTANCE.newInst(CustomData.class, "chickenextensions:playerlist");
-        packet.set(new String[] {s});
         PacketHelper.INSTANCE.send((AbstractPacket) packet);
     }
 
@@ -37,7 +38,7 @@ public class CustomPackerSender implements PacketRegister {
     public void registerPackets(QuadConsumer<Integer, Boolean, Boolean, Class<? extends AbstractPacket>> quadConsumer, Map<String, BiConsumer<PlayerBase, CustomData>> map) {
         map.put("playerlist", this::handleSendPlayers);
         map.put("playerlistResult", this::handleSendRes);
-        System.out.println("R u good boy?");
+        System.out.println(ModIDRegistry.packet.get("chickenextensions").get("playerlist").getClass().getName());
     }
 
     public void handleSendPlayers(PlayerBase playerBase, CustomData customData)
@@ -45,30 +46,24 @@ public class CustomPackerSender implements PacketRegister {
         if(FabricLoader.getInstance().getEnvironmentType() == EnvType.SERVER)
         {
             CustomData packet = GeneralFactory.INSTANCE.newInst(CustomData.class, "chickenextensions:playerlistResult");
-            packet.set(getPlayerNickList(minecraftServer.serverPlayerConnectionManager.players));
-            PacketHelper.INSTANCE.sendTo(getPlayerBase(customData), (AbstractPacket) packet);
+            packet.set(getPlayerNickList(((MinecraftServer) FabricLoader.getInstance().getGameInstance()).serverPlayerConnectionManager.players));
+            PacketHelper.INSTANCE.sendTo(playerBase, (AbstractPacket) packet);
         }
-    }
-
-    @Environment(EnvType.SERVER)
-    public PlayerBase getPlayerBase(CustomData customData)
-    {
-       return minecraftServer.serverPlayerConnectionManager.getServerPlayer(((String) customData.objects()[0]));
     }
 
     public void handleSendRes(PlayerBase playerBase, CustomData customData)
     {
-        staticPlayerList = (String[]) customData.objects();
+        staticPlayerList = customData.strings();
     }
 
 
     public String[] getPlayerNickList(List players)
     {
-        String[] lol = new String[players.size()];
-        for(int i = 0; i < lol.length; i++)
+        String[] toSend = new String[players.size()];
+        for(int i = 0; i < toSend.length; i++)
         {
-            lol[i] = ((PlayerBase)players.get(i)).name;
+            toSend[i] = ((PlayerBase)players.get(i)).name;
         }
-        return lol;
+        return toSend;
     }
 }
--- src/main/java/kz/chesschicken/chickenextensions/api/client/GuiPlayerList.java
+++ src/main/java/kz/chesschicken/chickenextensions/api/client/GuiPlayerList.java
@@ -2,7 +2,6 @@ package kz.chesschicken.chickenextensions.api.client;
 
 import net.minecraft.client.gui.screen.ScreenBase;
 import net.minecraft.client.gui.widgets.Button;
-import net.minecraft.client.resource.language.TranslationStorage;
 import org.lwjgl.input.Keyboard;
 
 public class GuiPlayerList extends ScreenBase {
@@ -24,11 +23,8 @@ public class GuiPlayerList extends ScreenBase {
     }
 
     public void init() {
-        TranslationStorage var1 = TranslationStorage.getInstance();
         Keyboard.enableRepeatEvents(true);
         this.buttons.clear();
-        this.buttons.add(new Button(0, this.width / 2 - 100, this.height / 4 + 96 + 12, var1.translate("multiplayer.connect")));
-        this.buttons.add(new Button(1, this.width / 2 - 100, this.height / 4 + 120 + 12, var1.translate("gui.cancel")));
 
     }
 
@@ -36,16 +32,6 @@ public class GuiPlayerList extends ScreenBase {
         Keyboard.enableRepeatEvents(false);
     }
 
-    protected void buttonClicked(Button button) {
-        if (button.active) {
-            if (button.id == 1) {
-                this.minecraft.openScreen(null);
-            } else if (button.id == 0) {
-
-            }
-
-        }
-    }
 
 
     protected void keyPressed(char character, int key) {
@@ -59,14 +45,13 @@ public class GuiPlayerList extends ScreenBase {
     }
 
     public void render(int mouseX, int mouseY, float delta) {
-        TranslationStorage var4 = TranslationStorage.getInstance();
         this.renderBackground();
-        this.drawTextWithShadowCentred(this.textManager, var4.translate("multiplayer.title"), this.width / 2, this.height / 4 - 60 + 20, 16777215);
+        this.drawTextWithShadowCentred(this.textManager, "Server list", this.width / 2, this.height / 4 - 60 + 20, 16777215);
 
         if(canRender)
         for(int i = 0; i < CustomPackerSender.staticPlayerList.length; i++)
         {
-            this.drawTextWithShadow(textManager, CustomPackerSender.staticPlayerList[i], 10, 10 + (i * 10), 16777215);
+            this.drawTextWithShadow(textManager, CustomPackerSender.staticPlayerList[i], this.width / 2, this.height / 4 - 60 + 20 + (i * 10), 16777215);
         }
         super.render(mouseX, mouseY, delta);
     }
